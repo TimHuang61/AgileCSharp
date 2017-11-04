8e82567c7cd78c3cd6cefcc2f535eee8dc215162
Tim <tim.y.huang@hotmail.com>
2017-11-04 23:38:03 +0800

    add demo_LSP project

---
+++ Demo_LSP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Read();
+        }
+    }
+}
